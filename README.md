b304d54bca9230acf1df4d1538dc3755feed4271
rijal2 <merentas5benua27@gmail.com>
2022-02-13 23:56:49 +0700

    02. Merapikan dan menginstall view engine ejs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WebServer-ExpressJS-01-ViewEngine-ejs-
-Membuat Template untuk tampilan Web
+Membuat Template untuk tampilan Web.
+Pada latihan ini template engine yang akan digunakan adalah ejs (Embeded Javascript templates).
+Dukumentasinya bisa dilihat di:
+1. npmjs.com/package/ejs
+2. ejs.co
+
+
--- app.js
+++ app.js
@@ -3,22 +3,19 @@ const app = express()
 const port = 3000
 
 
-//Setting halaman root
+//Setting halaman root (home)
 app.get('/', (req, res) => {
-//   res.send('Hello World! Ini adalah halaman root')
     res.sendFile('./index.html', {root: __dirname})
 })
 
 //Setting halaman about
 app.get('/about', (req, res) => {
-//   res.send('Hello World! Ini adalah halaman about')
     res.sendFile('./about.html', {root: __dirname})
 })
 
 //Setting halaman Contact
 app.get('/contact', (req, res) => {
-//   res.send('Hello World! Ini adalah halaman contact')
-res.sendFile('./contact.html', {root: __dirname})
+    res.sendFile('./contact.html', {root: __dirname})
 })
 
 //Latihan menggunakan metode rq.params. Penggunan metode ini akan menangkap request URL yang memiliki pola seperti ini: http://localhost:3000/produk/8/ctegory/rumah
@@ -41,43 +38,3 @@ app.use('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-/*
-const http = require('http');//memanggil Core Module http yang ada di NodeJS
-const { debug } = require('util');
-const port = 3000;
-const fs = require('fs');
-
-const renderHTML = (path, res) => {
-    fs.readFile(path, ((err, data) => {//Parameter 'err' akan mengirimkan informasi file html yang dicari apakah file nya ada atau tidak. Jika tidak ada maka parameter 'err' akan berisi error.
-        if(err){//Jika err bernilai error/true
-            res.writeHead(404);
-            res.write(`Error: File yang anda cari tidak ditemukan`)
-        } else{//Jika file nya ada / tidak error
-            res.write(data);//parameter 'data' berisi data file html
-        }
-        res.end()
-    }))
-}
-
-//Membuat server cengan methode CreateServer(), kemudian jalankan dengan methode listen()
-http.createServer((req, res) => {
-    const url = req.url;//Menangkap request dari user yang berupa URL
-    
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-
-    if(url === '/about'){
-        renderHTML('./about.html', res)    
-    } else if(url === '/contact'){
-        renderHTML('./contact.html', res)
-    } else{
-        renderHTML('./index.html', res)
-    }
-    
-}).listen(port, () => {
-    console.log(`Server sedang berjalan pada port ${port}..`)//akan dicetak pada terminal
-})
-
-*/
